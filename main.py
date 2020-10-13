f72fdf9f47bb1b108edbd6c2de2b8b82dace77da
Mateusz Filipek <neonowydroid@gmail.com>
2020-10-13 13:28:53 +0200

    passing param

--- main.py
+++ main.py
@@ -18,15 +18,29 @@ import json
 '''
 
 # currency exchange
-base_currency = input("Wprowadz skrot waluty bazowej: ").upper()
-target_currency = input("Wprowadz skrot waluty docelowej: ").upper()
+base_currency = input("Wprowadz skrot waluty bazowej: ").upper().rstrip()
+target_currency = input("Wprowadz skrot waluty docelowej: ").upper().rstrip()
+
+parameters = {
+  "base": base_currency
+}
 # base_currency, target_currency = "USD", "PLN"
-exchange_rate = requests.get("https://api.exchangeratesapi.io/latest?base={}&symbols={}".format(base_currency, target_currency))
+exchange_rate = requests.get("https://api.exchangeratesapi.io/latest?base={}&symbols={}".format(base_currency, target_currency))  # modyfing link
+exchange_rate_p = requests.get("https://api.exchangeratesapi.io/latest", params=parameters) # passing parameteres to request
 # print(json.dumps(exchange_rate.json(), sort_keys=True))
 # print(exchange_rate.json()["rates"])
+
 try: 
     for key, val in exchange_rate.json()["rates"].items():
         # print(key, val)
         print("1 {} - {} {}".format(base_currency, val, key))
 except KeyError:
     print("invalid input")
+
+try:
+  for key, val in exchange_rate_p.json()["rates"].items():
+    if key == target_currency:
+      print("1 {} - {} {}".format(base_currency, val, key))
+except KeyError:
+  print("invalid input")
+
